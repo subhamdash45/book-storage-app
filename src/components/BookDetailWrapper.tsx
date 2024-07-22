c662b1ed3b64a73392c31d0404639554db1c8078
subhamdash45 <mrsubhamdash1998@gmail.com>
2024-07-23 01:38:15 +0530

    chore(book-storage-app): updated linting problem

--- src/components/BookDetailWrapper.tsx
+++ src/components/BookDetailWrapper.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useLocation, useNavigate, useParams } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { Book } from "../types";
 import { BookDetail } from "./BookDetails";
 
