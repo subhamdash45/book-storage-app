28d56c36264666529512bb3c0735c00dcfedb3f0
subhamdash45 <mrsubhamdash1998@gmail.com>
2024-07-24 00:11:00 +0530

    fix(book-storage-app): fixed deployment issue and added suspense

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,16 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { App } from "./App";
 import "./global.scss";
 import { SnackbarProvider } from "notistack";
+import { Suspense } from "react";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <QueryClientProvider client={queryClient}>
     <SnackbarProvider>
-      <App />
+      <Suspense fallback={<div>Loading...</div>}>
+        <App />
+      </Suspense>
     </SnackbarProvider>
-  </QueryClientProvider>
+  </QueryClientProvider>,
 );
